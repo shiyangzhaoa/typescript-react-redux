835950e9e6ebc3baeaa8ff1ea5e6bb8f3d3493f0
杨钊 <zhao.yang@eoitek.com>
2018-03-26 16:06:49 +0800

    优化saga

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,4 +1,4 @@
-import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE } from '../constants';
+import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGIN_OUT } from '../constants';
 import { StoreState } from '../types';
 import { LoginStatus } from '../actions';
 
@@ -7,9 +7,11 @@ export function enthusiasm(state: StoreState, action: LoginStatus): StoreState {
     case LOGIN_REQUEST: 
       return { ...state, isLoginPending: true };
     case LOGIN_SUCCESS:
-    return { ...state, isLoginPending: false, token: action.token };
+    return { ...state, isLoginPending: false, token: action.token, error: undefined };
     case LOGIN_FAILURE:
       return { ...state, isLoginPending: false, error: action.error };
+    case LOGIN_OUT:
+      return { ...state, isLoginPending: false, error: undefined, token: undefined };
     default:
       return state;
   }
--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,5 +1,5 @@
 import { take, all, fork, select, call, put } from 'redux-saga/effects';
-import {  LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE } from '../constants';
+import {  LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGIN_OUT } from '../constants';
 import { api } from '../services';
 
 interface Login {
@@ -18,7 +18,7 @@ function* watchAndLog() {
 
 function* authorize(loginname: string, password: string) {
   try {
-    const {token} = yield call(api.userLogin, { loginname, password });
+    const { token } = yield call(api.userLogin, { loginname, password });
     yield put({type: LOGIN_SUCCESS, isLoginPending: false, token });
     return token;
   } catch (error) {
@@ -28,8 +28,13 @@ function* authorize(loginname: string, password: string) {
 
 function* loginFlow() {
   while (true) {
-    const {loginname, password} = (yield take(LOGIN_REQUEST)) as Login;
-    yield authorize(loginname, password);
+    const { loginname, password } = (yield take(LOGIN_REQUEST)) as Login;
+    const token = yield call(authorize, loginname, password);
+    if (token) {
+      localStorage.setItem('SAGA-TOKEN', token);
+      yield take(LOGIN_OUT);
+      localStorage.removeItem('SAGA-TOKEN');
+    }
   }
 }
 
